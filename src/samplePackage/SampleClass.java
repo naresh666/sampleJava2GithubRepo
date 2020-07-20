b042e05b0939897a509e425722b139bfbb9c3a84
naresh112 <kumar.naresh155@gmail.com>
2020-07-20 15:47:42 +0530

    3rd commit

--- src/samplePackage/SampleClass.java
+++ src/samplePackage/SampleClass.java
@@ -5,6 +5,7 @@ public class SampleClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("please cooperate git");
+		System.out.println("please cooperate git");
 
 	}
 
