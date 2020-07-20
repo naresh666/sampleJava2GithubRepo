15555df4a1e9c02086bad41f45ea671e066d1b1a
naresh112 <kumar.naresh155@gmail.com>
2020-07-20 15:43:41 +0530

    2nd commit from proj directory

---
+++ src/samplePackage/SampleClass.java
@@ -0,0 +1,11 @@
+package samplePackage;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("please cooperate git");
+
+	}
+
+}
